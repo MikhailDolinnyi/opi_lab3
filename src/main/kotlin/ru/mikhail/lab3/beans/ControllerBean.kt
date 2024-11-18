0961d9a7a974cbb51e030a6eba0880f2b10d6d7b
MikhailDolinnyi <dolinnyimikhail@gmail.com>
2024-11-18 17:41:37 +0300

    add validation on backend

--- src/main/kotlin/ru/mikhail/lab3/beans/ControllerBean.kt
+++ src/main/kotlin/ru/mikhail/lab3/beans/ControllerBean.kt
@@ -1,30 +1,38 @@
 package ru.mikhail.lab3.beans
 
+import jakarta.annotation.ManagedBean
 import jakarta.enterprise.context.SessionScoped
 import jakarta.inject.Inject
 import jakarta.inject.Named
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotNull
 import ru.mikhail.lab3.dbcommunication.ResultService
 import ru.mikhail.lab3.dbobjects.Result
 import java.io.Serializable
 
 @Named("controllerBean")
 @SessionScoped
+@ManagedBean
 open class ControllerBean : IControllerBean, Serializable { // Реализуем интерфейс
 
     override var x: Float = 0f
+    @NotNull @field:Max(2) @field:Min(-2)
     override var y: Float = 0f
+    @NotNull @field:Max(3) @field:Min(-5)
     override var r: Float = 0f
+    @NotNull @field:Max(3) @field:Min(1)
 
-    private val resultServiceKt = ResultService()
+    private val resultService = ResultService()
 
     @Inject
     private lateinit var _dotCheckerBean: IDotCheckerBean // Инжектируем интерфейс
 
     override fun completeRequest() {
-        resultServiceKt.saveResult(_dotCheckerBean.checkAndCalculatePoint())
+        resultService.saveResult(_dotCheckerBean.checkAndCalculatePoint())
     }
 
     override fun getResultList(): List<Result> {
-        return resultServiceKt.findAllResults()
+        return resultService.findAllResults()
     }
 }
