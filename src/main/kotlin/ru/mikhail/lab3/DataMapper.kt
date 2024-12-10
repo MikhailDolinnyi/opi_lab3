df6be42ee6f7292a53d81845afcddb6590928396
MikhailDolinnyi <dolinnyimikhail@gmail.com>
2024-12-10 21:44:13 +0300

    upgrade logic & move methods

--- src/main/kotlin/ru/mikhail/lab3/DataMapper.kt
+++ src/main/kotlin/ru/mikhail/lab3/DataMapper.kt
@@ -2,7 +2,6 @@ package ru.mikhail.lab3
 
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.inject.Named
-import org.mapstruct.Mapper
 import ru.mikhail.lab3.dbobjects.Result
 import ru.mikhail.lab3.dto.ResponseData
 
@@ -10,8 +9,8 @@ import ru.mikhail.lab3.dto.ResponseData
 @ApplicationScoped
 open class DataMapper {
 
-    open fun getDataList(result: List<Result>): List<ResponseData>{
-        return result.map{ item ->
+    open fun getDataList(result: List<Result>): List<ResponseData> {
+        return result.map { item ->
             ResponseData(item.x, item.y, item.r, item.result, item.executionTime, item.nowTime)
         }
     }
